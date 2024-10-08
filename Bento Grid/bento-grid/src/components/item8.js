802a4295b51c06756c58731376f4b81a49e2e5b0
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2024-10-08 16:43:37 +0200

    added item8

---
+++ Bento Grid/bento-grid/src/components/item8.js
@@ -0,0 +1,13 @@
+import Img from "../img/illustration-ai-content.webp";
+
+const item8 = () => {
+
+    return(
+        <div className="GridItem" id="Grid8">
+            <h2>Write your content using AI.</h2>
+            <img src={Img} alt="Description of image" />
+        </div>
+    );
+};
+
+export default item8;
