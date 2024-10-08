113652cfbf97fa1dca2e943392a70bffd9040626
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2024-10-08 15:09:29 +0200

    added basic structure

--- Bento Grid/bento-grid/src/App.js
+++ Bento Grid/bento-grid/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import './style.css';
+import Item1 from './components/item1';
+import Item2 from './components/item2';
+import Item3 from './components/item3';
+import Item4 from './components/item4';
+import Item5 from './components/item5';
+import Item6 from './components/item6';
+import Item7 from './components/item7';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="GridMain">
+        <div className="GridLeft">
+          <Item6/>
+          <Item7/>
+        </div>
+        <div className="GridRight">
+          <Item1/>
+          <Item2/>
+          <Item3/>
+          <Item4/>
+          <Item5/>
+        </div>
+      </div>
     </div>
   );
 }
